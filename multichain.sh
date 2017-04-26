99ab7e3982725aa25f516d4cccc8381fa8302a45
Rohas Nagpal <rohasnagpal@gmail.com>
2017-04-26 14:15:54 +0530

    Update multichain.sh

--- multichain.sh
+++ multichain.sh
@@ -4,3 +4,5 @@ wget http://www.multichain.com/download/multichain-1.0-beta-1.tar.gz
 tar -xvzf multichain-1.0-beta-1.tar.gz
 cd multichain-1.0-beta-1
 mv multichaind multichain-cli multichain-util /usr/local/bin
+multichain-util create chain1
+multichaind chain1 -daemon
